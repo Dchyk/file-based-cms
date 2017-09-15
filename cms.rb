912eea422a24f4e4afa99aeda4e91608e1e06ae1
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2017-09-14 20:57:24 -0400

    CMS updates

--- cms.rb
+++ cms.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sinatra/reloader'
 require 'tilt/erubis'
 
+root = File.expand_path("..", __FILE__)
+
 get "/" do
-  "Getting started."
+  @files = Dir.glob(root + "/data/*").map do |filename|
+    File.basename(filename)
+  end
+  erb :index
 end
+
+
+
+#get "/:file_name" do
+#  @content = File.read(root + "/data/#{params[:file_name]}.txt")
+#
+#  erb :file_content, layout: :index
+#end
